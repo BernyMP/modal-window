95ea3d3db92fbbdf853bff328a1d84f74b7afe50
Berny Peralta <bernardoperalta260@gmail.com>
2022-11-06 15:43:11 -0700

    finish logic of modal and added file to gitignore

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-
+const messages = ['Hello babe!', 'I love you', 'Buy me an acai bowl NOW'];
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -14,9 +14,16 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
+    document.getElementById('message').innerHTML = messages[i];
   });
 
   btnCloseModal.addEventListener('click', closeModal);
 }
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
